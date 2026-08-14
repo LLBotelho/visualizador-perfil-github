c416d8c715c73581e7445ca23b8f0de8c93a1ef7
Lucas Botelho <lucasbotelho121@gmail.com>
2026-08-13 22:48:40 -0300

    feat: adiciona verificação  caso não tenha nome

--- src/js/profileView.js
+++ src/js/profileView.js
@@ -18,7 +18,7 @@ export function renderProfile(userData, userRepos, container) {
         <div class="profile-card">
             <img src="${userData.avatar_url}" alt="Avatar de ${userData.name}" class="profile-avatar">
             <div class="profile-info">
-                <h2>${userData.name}</h2>
+                <h2>${userData.name || 'Sem nome 😢'}</h2>
                 <p>${userData.bio || 'Sem biografia 😢.'}</p>
             </div>
         </div>

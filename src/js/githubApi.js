12684eca2ce1bf7ac6084c08d2a92f781b2a0ca0
Lucas Botelho <lucasbotelho121@gmail.com>
2026-08-13 22:31:18 -0300

    feat: adiciona lista com 10 últimos repositórios

--- src/js/githubApi.js
+++ src/js/githubApi.js
@@ -1,8 +1,17 @@
+const baseURL = 'https://api.github.com';
+
 export async function fetchGithubUser(userName) {
-    const baseURL = 'https://api.github.com';
     const response = await fetch(`${baseURL}/users/${userName}`);
     if (!response.ok) {
         throw new Error('Usuario nao encontrado.');
+    }
+    return await response.json();
+}
+
+export async function fetchGithubRepos(userName){
+    const response = await fetch(`${baseURL}/users/${userName}/repos?per_page=10&sort=created`);
+    if (!response.ok) {
+        throw new Error('Repositorios nao encontrados.');
     }
     return await response.json();
 }
